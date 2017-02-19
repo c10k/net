f3d9d33162aad1ad76bdfaa63578b0233daebd57
tushar khurana <tusharkhurana99@gmail.com>
2017-02-19 14:32:43 +0530

    Refactor error handling, add initial support for AF_UNIX (#2)
    * Try to complete all TODO's
    
    * Remove static_cast from errno comparisons
    
    * Add code for capturing current errno value in currentErrNo variable
    
    * Try to handle error in non-blocking sockets using bool *_isErrorInNonBlocking in read and recv
    
    * Make getErrorMsg func arg const
    
    * Rename isErrorInNonBlocking to isSpecialErrorInNonBlocking
    
    * Refactor error handling, add initial support for AF_UNIX

--- src/socket.cpp
+++ src/socket.cpp
@@ -25,11 +25,20 @@ void Socket::bind(const char _addr[], const int _port) const
 			}
 		} break;
 
+		case SF::domain::UNIX: {
+			sockaddr_un servaddr;
+			res = net::method::construct(servaddr, _addr);
+			unlink(servaddr.sun_path);
+			res = ::bind(sockfd, (sockaddr *) &servaddr, sizeof(servaddr));
+			res = (res == 0) ? 1 : res;
+		} break;
+
 		default: throw std::logic_error("Protocol yet not implemented");
 	}
 
+	const auto currErrno = errno;
 	if (res == -1) {
-		throw std::runtime_error(net::method::getErrorMsg());
+		throw std::runtime_error(net::method::getErrorMsg(currErrno));
 	} else if (res == 0) {
 		throw std::invalid_argument("Address argument invalid");
 	}
@@ -41,9 +50,10 @@ void Socket::start(const char _addr[], const int _port, const int _q) const
 	try {
 		bind(_addr, _port);
 
-		if (type == SF::type::TCP || type == SF::type::SCTP) {
+		if (type == SF::type::TCP || type == SF::type::SEQPACKET) {
 			if (listen(sockfd, _q) < 0) {
-				throw std::runtime_error(net::method::getErrorMsg());
+				const auto currErrno = errno;
+				throw std::runtime_error(net::method::getErrorMsg(currErrno));
 			}
 		}
 	} catch (...) {
@@ -52,7 +62,7 @@ void Socket::start(const char _addr[], const int _port, const int _q) const
 }
 
 
-void Socket::connect(const char _addr[], const int _port) const
+void Socket::connect(const char _addr[], const int _port, bool *_errorNB) const
 {
 	auto res = -1;
 	switch (domain) {
@@ -74,21 +84,34 @@ void Socket::connect(const char _addr[], const int _port) const
 			}
 		} break;
 
+		case SF::domain::UNIX: {
+			sockaddr_un addr;
+			res = net::method::construct(addr, _addr);
+			res = ::connect(sockfd, (sockaddr *) &addr, sizeof(addr));
+			res = (res == 0) ? 1 : res;
+		} break;
+
 		default: throw std::logic_error("Protocol yet not implemented");
 	}
 
-	// TODO: Check for error == EINPROGRESS and don't throw exception in that
-	// case.
-
+	const auto currErrno = errno;
 	if (res == -1) {
-		throw std::runtime_error(net::method::getErrorMsg());
+		if (currErrno == EINPROGRESS) {
+			if (_errorNB != nullptr) {
+				*_errorNB = true;
+			} else {
+				throw std::invalid_argument("errorNB argument missing");
+			}
+		} else {
+			throw std::runtime_error(net::method::getErrorMsg(currErrno));
+		}
 	} else if (res == 0) {
 		throw std::invalid_argument("Address argument invalid");
 	}
 }
 
 
-Socket Socket::accept() const
+Socket Socket::accept(bool *_errorNB) const
 {
 	auto peerAddr = std::make_unique<sockaddr_storage>();
 
@@ -97,84 +120,103 @@ Socket Socket::accept() const
 
 	auto client = ::accept(sockfd, (sockaddr *) peerAddr.get(), &peerAddrSize);
 
+	const auto currErrno = errno;
 	if (client == -1) {
-		// TODO: Check for client == EAGAIN or EWOULDBLOCK and
-		// don't throw exception in that case.
-		throw std::runtime_error(net::method::getErrorMsg());
+		if (currErrno == EAGAIN || currErrno == EWOULDBLOCK) {
+			if (_errorNB != nullptr) {
+				*_errorNB = true;
+			} else {
+				throw std::invalid_argument("errorNB argument missing");
+			}
+		} else {
+			throw std::runtime_error(net::method::getErrorMsg(currErrno));
+		}
 	}
 
 	return Socket(client, peerAddr);
 }
 
 
-void Socket::write(const std::string &_msg) const
+void Socket::write(const std::string &_msg, bool *_errorNB) const
 {
 	const auto written = low_write(::write, sockfd, _msg);
 
+	const auto currErrno = errno;
 	if (written == -1) {
-		// TODO: Check for written == EAGAIN or EWOULDBLOCK and
-		// don't throw exception in that case.
-		throw std::runtime_error(net::method::getErrorMsg());
-	}
-}
-
-
-void Socket::send(const std::string &_msg, SF::send _flags) const
-{
-	const auto flags = static_cast<int>(_flags);
-	const auto sent  = low_write(::send, sockfd, _msg, flags);
-
-	if (sent == -1) {
-		// TODO: Check for sent == EAGAIN or EWOULDBLOCK and
-		// don't throw exception in that case.
-		throw std::runtime_error(net::method::getErrorMsg());
+		if (currErrno == EAGAIN || currErrno == EWOULDBLOCK) {
+			if (_errorNB != nullptr) {
+				*_errorNB = true;
+			} else {
+				throw std::invalid_argument("errorNB argument missing");
+			}
+		} else {
+			throw std::runtime_error(net::method::getErrorMsg(currErrno));
+		}
 	}
 }
 
 
 void Socket::send(
-  const std::string &_msg, sockaddr_storage &_addr, SF::send _flags) const
+  const std::string &_msg, SF::send _flags, bool *_errorNB) const
+{
+	const auto flags = static_cast<int>(_flags);
+	const auto sent  = low_write(::send, sockfd, _msg, flags);
+
+	const auto currErrno = errno;
+	if (sent == -1) {
+		if (currErrno == EAGAIN || currErrno == EWOULDBLOCK) {
+			if (_errorNB != nullptr) {
+				*_errorNB = true;
+			} else {
+				throw std::invalid_argument("errorNB argument missing");
+			}
+		} else {
+			throw std::runtime_error(net::method::getErrorMsg(currErrno));
+		}
+	}
+}
+
+
+void Socket::send(const std::string &_msg, sockaddr_storage &_addr,
+  SF::send _flags, bool *_errorNB) const
 {
 	const auto flags = static_cast<int>(_flags);
 	const auto sent  = low_write(
 	  ::sendto, sockfd, _msg, flags, (sockaddr *) &_addr, sizeof(_addr));
 
+	const auto currErrno = errno;
 	if (sent == -1) {
-		// TODO: Check for sent == EAGAIN or EWOULDBLOCK and
-		// don't throw exception in that case.
-		throw std::runtime_error(net::method::getErrorMsg());
+		if (currErrno == EAGAIN || currErrno == EWOULDBLOCK) {
+			if (_errorNB != nullptr) {
+				*_errorNB = true;
+			} else {
+				throw std::invalid_argument("errorNB argument missing");
+			}
+		} else {
+			throw std::runtime_error(net::method::getErrorMsg(currErrno));
+		}
 	}
 }
 
 
-std::string Socket::read(const int _bufSize) const
+std::string Socket::read(const int _bufSize, bool *_errorNB) const
 {
 	std::string str;
 	str.reserve(_bufSize);
 
 	const auto recvd = low_read(::read, sockfd, str);
 
+	const auto currErrno = errno;
 	if (recvd == -1) {
-		// TODO: Check for bytes == EAGAIN or EWOULDBLOCK and
-		// don't throw exception in that case.
-		throw std::runtime_error(net::method::getErrorMsg());
-	}
-	return str;
-}
-
-
-std::string Socket::recv(const int _bufSize, SF::recv _flags) const
-{
-	std::string str;
-	str.reserve(_bufSize);
-
-	const auto flags = static_cast<int>(_flags);
-	const auto recvd = low_read(::recv, sockfd, str, flags);
-
-	if (recvd == -1) {
-		// TODO: Check for recvd == EAGAIN or EWOULDBLOCK and
-		// don't throw exception in that case.
-		throw std::runtime_error(net::method::getErrorMsg());
+		if (currErrno == EAGAIN || currErrno == EWOULDBLOCK) {
+			if (_errorNB != nullptr) {
+				*_errorNB = true;
+			} else {
+				throw std::invalid_argument("errorNB argument missing");
+			}
+		} else {
+			throw std::runtime_error(net::method::getErrorMsg(currErrno));
+		}
 	}
 
 	return str;
@@ -182,7 +224,33 @@ std::string Socket::recv(const int _bufSize, SF::recv _flags) const
 
 
 std::string Socket::recv(
-  sockaddr_storage &_addr, const int _bufSize, SF::recv _flags) const
+  const int _bufSize, SF::recv _flags, bool *_errorNB) const
+{
+	std::string str;
+	str.reserve(_bufSize);
+
+	const auto flags = static_cast<int>(_flags);
+	const auto recvd = low_read(::recv, sockfd, str, flags);
+
+	const auto currErrno = errno;
+	if (recvd == -1) {
+		if (currErrno == EAGAIN || currErrno == EWOULDBLOCK) {
+			if (_errorNB != nullptr) {
+				*_errorNB = true;
+			} else {
+				throw std::invalid_argument("errorNB argument missing");
+			}
+		} else {
+			throw std::runtime_error(net::method::getErrorMsg(currErrno));
+		}
+	}
+
+	return str;
+}
+
+
+std::string Socket::recv(sockaddr_storage &_addr, const int _bufSize,
+  SF::recv _flags, bool *_errorNB) const
 {
 	std::string str;
 	str.reserve(_bufSize);
@@ -192,10 +260,17 @@ std::string Socket::recv(
 	const auto recvd
 	  = low_read(::recvfrom, sockfd, str, flags, (sockaddr *) &_addr, &length);
 
+	const auto currErrno = errno;
 	if (recvd == -1) {
-		// TODO: Check for recvd == EAGAIN or EWOULDBLOCK and
-		// don't throw exception in that case.
-		throw std::runtime_error(net::method::getErrorMsg());
+		if (currErrno == EAGAIN || currErrno == EWOULDBLOCK) {
+			if (_errorNB != nullptr) {
+				*_errorNB = true;
+			} else {
+				throw std::invalid_argument("errorNB argument missing");
+			}
+		} else {
+			throw std::runtime_error(net::method::getErrorMsg(currErrno));
+		}
 	}
 
 	return str;
