365cdd3e7f8322784e5fd7eb29c57d43dc5815b4
tkhurana <tusharkhurana99@gmail.com>
2017-03-03 03:45:33 +0530

    Try to add some constructor testing of Socket

---
+++ test/someTesting.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <cstring>
+
+int main()
+{
+	for (int i = 0; i < 255; i++) {
+		std::cout << "i:: " << i;
+		auto res = socket(AF_INET, SOCK_DGRAM, 6);
+		if (res < 0) {
+			std::cout << "  Error in creating socket: " << strerror(errno);
+		}
+		std::cout << '\n';
+	}
+	return 0;
+}
