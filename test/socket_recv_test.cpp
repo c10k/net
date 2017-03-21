eda845b173b7af6169014e16a2b4a72e295cb1f6
tkhurana <tusharkhurana99@gmail.com>
2017-03-21 22:59:39 +0530

    Revamp socket_send_test.cpp and socket_recv_test.cpp

--- test/socket_recv_test.cpp
+++ test/socket_recv_test.cpp
@@ -7,149 +7,149 @@
 using namespace net;
 using namespace std::chrono_literals;
 
-auto msgLen2 = 15000;
-std::string msg1(msgLen2, 'a');
-std::string msg2(msgLen2, 'b');
-std::string msg3(msgLen2, 'c');
-
 
 namespace recvTest {
+
+const auto msgLen2 = 15000;
+const std::string msg1(recvTest::msgLen2, 'a');
+const std::string msg2(recvTest::msgLen2, 'b');
+const std::string msg3(recvTest::msgLen2, 'c');
+
+
 void udpIPv4ServerProcessing(Socket &serverSocket)
 {
-	for (int i = 1; i <= 1; i++) {
-		auto res = serverSocket.recv(msgLen2, [](AddrIPv4 &s) {
-			std::string str(100, ' ');
-			auto ptr = inet_ntop(AF_INET, &s.sin_addr.s_addr, &str.front(),
-			                     str.size());
-			if (ptr != nullptr) {
-				str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
-				ASSERT_EQ(str.substr(0, str.size() - 1), "127.0.0.1");
-			} else {
-				throw std::invalid_argument("Peer address not valid");
-			}
-		});
-	}
+	const auto res = serverSocket.recv(recvTest::msgLen2, [](AddrIPv4 &s) {
+		std::string str(100, ' ');
+		const auto ptr
+		  = inet_ntop(AF_INET, &s.sin_addr.s_addr, &str.front(), str.size());
+		if (ptr != nullptr) {
+			str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
+			ASSERT_EQ(str.substr(0, str.size() - 1), "127.0.0.1");
+		} else {
+			throw std::invalid_argument("Peer address not valid");
+		}
+	});
+	EXPECT_EQ(res, recvTest::msg1);
 }
 
 void udpIPv6ServerProcessing(Socket &serverSocket)
 {
-	for (int i = 1; i <= 1; i++) {
-		auto res = serverSocket.recv(msgLen2, [](AddrIPv6 &s) {
-			std::string str(100, ' ');
-			auto ptr = inet_ntop(AF_INET6, &s.sin6_addr.s6_addr, &str.front(),
-			                     str.size());
-			if (ptr != nullptr) {
-				str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
-				ASSERT_EQ(str.substr(0, str.size() - 1), "::1");
-			} else {
-				throw std::invalid_argument("Peer address not valid");
-			}
-		});
-	}
+	const auto res = serverSocket.recv(recvTest::msgLen2, [](AddrIPv6 &s) {
+		std::string str(100, ' ');
+		const auto ptr
+		  = inet_ntop(AF_INET6, &s.sin6_addr.s6_addr, &str.front(), str.size());
+		if (ptr != nullptr) {
+			str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
+			ASSERT_EQ(str.substr(0, str.size() - 1), "::1");
+		} else {
+			throw std::invalid_argument("Peer address not valid");
+		}
+	});
+	EXPECT_EQ(res, recvTest::msg1);
 }
 
-std::string getPeerIp(Socket &peer, Domain d)
+std::string getPeerIp(const Socket &peer, const Domain d)
 {
 	std::string actualIp(100, ' ');
-	struct sockaddr_in actualPeerIp;
+	sockaddr_in actualPeerIp;
 	socklen_t actualPeerIpLen = sizeof(actualPeerIp);
 
-	auto status = getpeername(peer.getSocket(), (sockaddr *) &actualPeerIp,
-	                          &actualPeerIpLen);
+	const auto status = getpeername(
+	  peer.getSocket(), (sockaddr *) &actualPeerIp, &actualPeerIpLen);
 
-	auto status2 = inet_ntop(static_cast<int>(d), &actualPeerIp.sin_addr.s_addr,
-	                         &actualIp.front(), actualIp.size());
+	const auto status2
+	  = inet_ntop(static_cast<int>(d), &actualPeerIp.sin_addr.s_addr,
+	              &actualIp.front(), actualIp.size());
 
-	if (status != -1 && status2 != nullptr) {
-		actualIp.erase(std::remove(actualIp.begin(), actualIp.end(), ' '),
-		               actualIp.end());
-		return actualIp;
-	} else
-		throw std::runtime_error("Error in getPeerIp");
+	if (status == -1) {
+		throw std::runtime_error("Error in getpeername in getPeerIp()");
+	}
+	if (status2 == nullptr) {
+		throw std::runtime_error("Error in inet_ntop in getPeerIp()");
+	}
+
+	actualIp.erase(std::remove(actualIp.begin(), actualIp.end(), ' '),
+	               actualIp.end());
+	return actualIp;
 }
 
 void tcpIPv4ServerProcessing(Socket &serverSocket)
 {
-	for (int i = 1; i <= 1; i++) {
-		auto peer = serverSocket.accept();
+	const auto peer = serverSocket.accept();
 
-		auto msg = peer.read(msgLen2);
-		if (msg == msg1)
-			peer.send("msg1");
-		else
-			peer.send(" ");
+	const auto msg = peer.read(recvTest::msgLen2);
+	if (msg == recvTest::msg1) {
+		peer.send("recvTest::msg1");
+	} else {
+		peer.send(" ");
+	}
 
-		auto otherMsg = peer.recv(msgLen2, [&](AddrIPv4 &s) {
+	const auto otherMsg = peer.recv(recvTest::msgLen2, [&](AddrIPv4 &s) {
 
-			auto actualPeerIp = getPeerIp(peer, Domain::IPv4);
+		// const auto actualPeerIp = getPeerIp(peer, Domain::IPv4);
+		// EXPECT_EQ(actualPeerIp, "127.0.0.1");
 
-			std::string str(100, ' ');
-			auto ptr = inet_ntop(AF_INET, &s.sin_addr.s_addr, &str.front(),
-			                     str.size());
-			if (ptr != nullptr) {
-				str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
-				// The following statement is so bcoz, in case of recvfrom being
-				// used with TCP socket, recvfrom doesn't fill the passed
-				// AddrIPv4/AddrIPv6 structure and leaves it as it was passed to
-				// it.
-				EXPECT_NE(str.substr(0, str.size() - 1), "127.0.0.1");
-			} else {
-				throw std::invalid_argument("Peer address not valid");
-			}
-		});
+		std::string str(100, ' ');
+		const auto ptr
+		  = inet_ntop(AF_INET, &s.sin_addr.s_addr, &str.front(), str.size());
+		if (ptr != nullptr) {
+			str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
+			EXPECT_NE(str.substr(0, str.size() - 1), "127.0.0.1");
+		} else {
+			throw std::invalid_argument("Peer address not valid");
+		}
+	});
 
-		if (otherMsg == msg2)
-			peer.send("msg2");
-		else
-			peer.send(" ");
+	if (otherMsg == recvTest::msg2) {
+		peer.send("recvTest::msg2");
+	} else {
+		peer.send(" ");
+	}
 
-		auto yetAnotherMsg = peer.recv(msgLen2);
-		if (yetAnotherMsg == msg3)
-			peer.send("msg3");
-		else
-			peer.send(" ");
+	const auto yetAnotherMsg = peer.recv(recvTest::msgLen2);
+	if (yetAnotherMsg == recvTest::msg3) {
+		peer.send("recvTest::msg3");
+	} else {
+		peer.send(" ");
 	}
 }
 
 void tcpIPv6ServerProcessing(Socket &serverSocket)
 {
-	for (int i = 1; i <= 1; i++) {
-		auto peer = serverSocket.accept();
+	const auto peer = serverSocket.accept();
 
-		auto msg = peer.read(msgLen2);
-		if (msg == msg1)
-			peer.send("msg1");
-		else
-			peer.send(" ");
+	const auto msg = peer.read(recvTest::msgLen2);
+	if (msg == recvTest::msg1)
+		peer.send("recvTest::msg1");
+	else
+		peer.send(" ");
 
-		auto otherMsg = peer.recv(msgLen2, [&](AddrIPv6 &s) {
+	const auto otherMsg = peer.recv(recvTest::msgLen2, [&](AddrIPv6 &s) {
 
-			auto actualPeerIp = getPeerIp(peer, Domain::IPv6);
+		// const auto actualPeerIp = getPeerIp(peer, Domain::IPv6);
+		// EXPECT_EQ(actualPeerIp, "::1");
 
-			std::string str(100, ' ');
-			auto ptr = inet_ntop(AF_INET6, &s.sin6_addr.s6_addr, &str.front(),
-			                     str.size());
-			if (ptr != nullptr) {
-				str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
-				// The following statement is so bcoz, in case of recvfrom being
-				// used with TCP socket, recvfrom doesn't fill the passed
-				// AddrIPv4/AddrIPv6 structure and leaves it as it was passed to
-				// it.
-				EXPECT_NE(str.substr(0, str.size() - 1), "::1");
-			} else {
-				throw std::invalid_argument("Peer address not valid");
-			}
-		});
-		if (otherMsg == msg2)
-			peer.send("msg2");
-		else
-			peer.send(" ");
+		std::string str(100, ' ');
+		const auto ptr
+		  = inet_ntop(AF_INET6, &s.sin6_addr.s6_addr, &str.front(), str.size());
+		if (ptr != nullptr) {
+			str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
+			EXPECT_NE(str.substr(0, str.size() - 1), "::1");
+		} else {
+			throw std::invalid_argument("Peer address not valid");
+		}
+	});
+	if (otherMsg == recvTest::msg2) {
+		peer.send("recvTest::msg2");
+	} else {
+		peer.send(" ");
+	}
 
-		auto yetAnotherMsg = peer.recv(msgLen2);
-		if (yetAnotherMsg == msg3)
-			peer.send("msg3");
-		else
-			peer.send(" ");
+	const auto yetAnotherMsg = peer.recv(recvTest::msgLen2);
+	if (yetAnotherMsg == recvTest::msg3) {
+		peer.send("recvTest::msg3");
+	} else {
+		peer.send(" ");
 	}
 }
 
@@ -189,24 +189,24 @@ TEST(Socket, IPv4Recv)
 {
 	std::thread tcpServerThreadIPv4(recvTest::startTCPServerIPv4);
 	std::thread udpServerThreadIPv4(recvTest::startUDPServerIPv4);
-	std::this_thread::sleep_for(2s);
+	std::this_thread::sleep_for(1s);
 
-	Socket client1(Domain::IPv4, Type::TCP);
-	client1.connect("127.0.0.1", 17000);
+	Socket tcpClient1(Domain::IPv4, Type::TCP);
+	tcpClient1.connect("127.0.0.1", 17000);
 
-	EXPECT_NO_THROW(client1.send(msg1));
-	EXPECT_EQ(client1.read(4), "msg1");
+	EXPECT_NO_THROW(tcpClient1.send(recvTest::msg1));
+	EXPECT_EQ(tcpClient1.read(4), "recvTest::msg1");
 
-	EXPECT_NO_THROW(client1.send(msg2));
-	EXPECT_EQ(client1.read(4), "msg2");
+	EXPECT_NO_THROW(tcpClient1.send(recvTest::msg2));
+	EXPECT_EQ(tcpClient1.read(4), "recvTest::msg2");
 
-	EXPECT_NO_THROW(client1.send(msg3));
-	EXPECT_EQ(client1.read(4), "msg3");
+	EXPECT_NO_THROW(tcpClient1.send(recvTest::msg3));
+	EXPECT_EQ(tcpClient1.read(4), "recvTest::msg3");
 
-	client1.close();
+	tcpClient1.close();
 
 	Socket udpClient1(Domain::IPv4, Type::UDP);
-	EXPECT_NO_THROW(udpClient1.send(msg1, [](AddrIPv4 &s) {
+	EXPECT_NO_THROW(udpClient1.send(recvTest::msg1, [](AddrIPv4 &s) {
 		return methods::construct(s, "127.0.0.1", 17000);
 	}));
 
@@ -219,25 +219,26 @@ TEST(Socket, IPv6Recv)
 
 	std::thread tcpServerThreadIPv6(recvTest::startTCPServerIPv6);
 	std::thread udpServerThreadIPv6(recvTest::startUDPServerIPv6);
-	std::this_thread::sleep_for(2s);
+	std::this_thread::sleep_for(1s);
 
-	Socket client2(Domain::IPv6, Type::TCP);
-	client2.connect("::1", 18000);
+	Socket tcpClient2(Domain::IPv6, Type::TCP);
+	tcpClient2.connect("::1", 18000);
 
-	EXPECT_NO_THROW(client2.send(msg1));
-	EXPECT_EQ(client2.read(4), "msg1");
+	EXPECT_NO_THROW(tcpClient2.send(recvTest::msg1));
+	EXPECT_EQ(tcpClient2.read(4), "recvTest::msg1");
 
-	EXPECT_NO_THROW(client2.send(msg2));
-	EXPECT_EQ(client2.read(4), "msg2");
+	EXPECT_NO_THROW(tcpClient2.send(recvTest::msg2));
+	EXPECT_EQ(tcpClient2.read(4), "recvTest::msg2");
 
-	EXPECT_NO_THROW(client2.send(msg3));
-	EXPECT_EQ(client2.read(4), "msg3");
+	EXPECT_NO_THROW(tcpClient2.send(recvTest::msg3));
+	EXPECT_EQ(tcpClient2.read(4), "recvTest::msg3");
 
-	client2.close();
+	tcpClient2.close();
 
 	Socket udpClient2(Domain::IPv6, Type::UDP);
-	EXPECT_NO_THROW(udpClient2.send(
-	  msg1, [](AddrIPv6 &s) { return methods::construct(s, "::1", 18000); }));
+	EXPECT_NO_THROW(udpClient2.send(recvTest::msg1, [](AddrIPv6 &s) {
+		return methods::construct(s, "::1", 18000);
+	}));
 
 	tcpServerThreadIPv6.join();
 	udpServerThreadIPv6.join();
--- test/socket_send_test.cpp
+++ test/socket_send_test.cpp
@@ -7,69 +7,71 @@
 using namespace net;
 using namespace std::chrono_literals;
 
+
+namespace sendTest {
+
+const auto msgLen = 15000;
+const std::string someString(sendTest::msgLen, 'a');
+
 void udpIPv4ServerProcessing(Socket &serverSocket)
 {
-	for (int i = 1; i <= 1; i++) {
-		auto res = serverSocket.recv(15000, [](AddrIPv4 &s) {
-			std::string str(100, ' ');
-			auto ptr = inet_ntop(AF_INET, &s.sin_addr.s_addr, &str.front(),
-			                     str.size());
-			if (ptr != nullptr) {
-				str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
-				ASSERT_EQ(str.substr(0, str.size() - 1), "127.0.0.1");
-			} else {
-				throw std::invalid_argument("Peer address not valid");
-			}
-		});
-	}
+	const auto res = serverSocket.recv(15000, [](AddrIPv4 &s) {
+		std::string str(100, ' ');
+		const auto ptr
+		  = inet_ntop(AF_INET, &s.sin_addr.s_addr, &str.front(), str.size());
+		if (ptr != nullptr) {
+			str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
+			ASSERT_EQ(str.substr(0, str.size() - 1), "127.0.0.1");
+		} else {
+			throw std::invalid_argument("Peer address not valid");
+		}
+	});
+	EXPECT_EQ(res, sendTest::someString);
 }
 
 void udpIPv6ServerProcessing(Socket &serverSocket)
 {
-	for (int i = 1; i <= 1; i++) {
-		auto res = serverSocket.recv(15000, [](AddrIPv6 &s) {
-			std::string str(100, ' ');
-			auto ptr = inet_ntop(AF_INET6, &s.sin6_addr.s6_addr, &str.front(),
-			                     str.size());
-			if (ptr != nullptr) {
-				str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
-				ASSERT_EQ(str.substr(0, str.size() - 1), "::1");
-			} else {
-				throw std::invalid_argument("Peer address not valid");
-			}
-		});
-	}
+	const auto res = serverSocket.recv(15000, [](AddrIPv6 &s) {
+		std::string str(100, ' ');
+		const auto ptr
+		  = inet_ntop(AF_INET6, &s.sin6_addr.s6_addr, &str.front(), str.size());
+		if (ptr != nullptr) {
+			str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
+			ASSERT_EQ(str.substr(0, str.size() - 1), "::1");
+		} else {
+			throw std::invalid_argument("Peer address not valid");
+		}
+	});
+	EXPECT_EQ(res, sendTest::someString);
 }
 
 void tcpServerProcessing(Socket &serverSocket)
 {
-	for (int i = 1; i <= 1; i++) {
-		auto peer = serverSocket.accept();
+	const auto peer = serverSocket.accept();
 
-		auto recvd = 0;
-		while (!(recvd >= 15000)) {
-			auto msg = peer.read(1024);
-			recvd += msg.size();
-		}
-		auto response = std::to_string(recvd);
-		peer.send(response);
-
-		// recvd = 0;
-		// while (!(recvd >= 15000)) {
-		// 	auto msg = peer.read(1024);
-		// 	recvd += msg.size();
-		// }
-		// response = std::to_string(recvd);
-		// peer.send(response);
-
-		recvd = 0;
-		while (!(recvd >= 15000)) {
-			auto msg = peer.read(1024);
-			recvd += msg.size();
-		}
-		response = std::to_string(recvd);
-		peer.send(response);
+	auto recvd = 0;
+	while (!(recvd >= 15000)) {
+		auto msg = peer.read(1024);
+		recvd += msg.size();
 	}
+	auto response = std::to_string(recvd);
+	peer.send(response);
+
+	// recvd = 0;
+	// while (!(recvd >= 15000)) {
+	// 	auto msg = peer.read(1024);
+	// 	recvd += msg.size();
+	// }
+	// response = std::to_string(recvd);
+	// peer.send(response);
+
+	recvd = 0;
+	while (!(recvd >= 15000)) {
+		auto msg = peer.read(1024);
+		recvd += msg.size();
+	}
+	response = std::to_string(recvd);
+	peer.send(response);
 }
 
 void startTCPServerIPv6()
@@ -102,35 +104,33 @@ void startUDPServerIPv4()
 	udpIPv4ServerProcessing(myServerSocket);
 }
 
-auto msgLen = 15000;
-std::string someString(msgLen, 'a');
-
 TEST(Socket, IPv4Send)
 {
 	std::thread tcpServerThreadIPv4(startTCPServerIPv4);
 	std::thread udpServerThreadIPv4(startUDPServerIPv4);
-	std::this_thread::sleep_for(2s);
+	std::this_thread::sleep_for(1s);
 
-	Socket client1(Domain::IPv4, Type::TCP);
-	client1.connect("127.0.0.1", 15000);
-	EXPECT_NO_THROW(client1.send(someString));
-	EXPECT_EQ(client1.read(5), std::to_string(msgLen));
+	Socket tcpClient1(Domain::IPv4, Type::TCP);
+	tcpClient1.connect("127.0.0.1", 15000);
+	EXPECT_NO_THROW(tcpClient1.send(sendTest::someString));
+	EXPECT_EQ(tcpClient1.read(5), std::to_string(sendTest::msgLen));
 	// -- Resolve This -- and then accordingly change commented portion of tcp
 	// server.
-	// EXPECT_THROW(client1.send(someString, [](AddrIPv4 &s) { return 5; }),
+	// EXPECT_THROW(tcpClient1.send(sendTest::someString, [](AddrIPv4 &s) {
+	// return 5; }),
 	//              std::runtime_error);
-	// EXPECT_NE(client1.read(5), std::to_string(msgLen));
-	EXPECT_NO_THROW(client1.send(someString, [](AddrIPv4 &s) {
+	// EXPECT_NE(tcpClient1.read(5), std::to_string(sendTest::msgLen));
+	EXPECT_NO_THROW(tcpClient1.send(sendTest::someString, [](AddrIPv4 &s) {
 		s.sin_family      = AF_INET;
 		s.sin_port        = htons(15000);
 		s.sin_addr.s_addr = htonl(INADDR_ANY);
 		return 1;
 	}));
-	EXPECT_NE(client1.read(5), std::to_string(msgLen));
-	client1.close();
+	EXPECT_EQ(tcpClient1.read(5), std::to_string(sendTest::msgLen));
+	tcpClient1.close();
 
 	Socket udpClient1(Domain::IPv4, Type::UDP);
-	EXPECT_NO_THROW(udpClient1.send(someString, [](AddrIPv4 &s) {
+	EXPECT_NO_THROW(udpClient1.send(sendTest::someString, [](AddrIPv4 &s) {
 		return methods::construct(s, "127.0.0.1", 15000);
 	}));
 
@@ -143,31 +143,33 @@ TEST(Socket, IPv6Send)
 
 	std::thread tcpServerThreadIPv6(startTCPServerIPv6);
 	std::thread udpServerThreadIPv6(startUDPServerIPv6);
-	std::this_thread::sleep_for(2s);
+	std::this_thread::sleep_for(1s);
 
-	Socket client2(Domain::IPv6, Type::TCP);
-	client2.connect("::1", 16000);
-	EXPECT_NO_THROW(client2.send(someString));
-	EXPECT_EQ(client2.read(5), std::to_string(msgLen));
+	Socket tcpClient2(Domain::IPv6, Type::TCP);
+	tcpClient2.connect("::1", 16000);
+	EXPECT_NO_THROW(tcpClient2.send(sendTest::someString));
+	EXPECT_EQ(tcpClient2.read(5), std::to_string(sendTest::msgLen));
 	// -- Resolve This -- and then accordingly change commented portion of tcp
 	// server.
-	// EXPECT_THROW(client2.send(someString, [](AddrIPv6 &s) { return 5; }),
+	// EXPECT_THROW(tcpClient2.send(sendTest::someString, [](AddrIPv6 &s) {
+	// return 5; }),
 	//              std::runtime_error);
-	// EXPECT_NE(client2.read(5), std::to_string(msgLen));
-	EXPECT_NO_THROW(client2.send(someString, [](AddrIPv6 &s) {
-		s.sin6_family = AF_INET;
+	// EXPECT_NE(tcpClient2.read(5), std::to_string(sendTest::msgLen));
+	EXPECT_NO_THROW(tcpClient2.send(sendTest::someString, [](AddrIPv6 &s) {
+		s.sin6_family = AF_INET6;
 		s.sin6_port   = htons(16000);
 		s.sin6_addr   = in6addr_any;
 		return 1;
 	}));
-	EXPECT_NE(client2.read(5), std::to_string(msgLen));
-	client2.close();
+	EXPECT_EQ(tcpClient2.read(5), std::to_string(sendTest::msgLen));
+	tcpClient2.close();
 
 	Socket udpClient2(Domain::IPv6, Type::UDP);
-	EXPECT_NO_THROW(udpClient2.send(someString, [](AddrIPv6 &s) {
+	EXPECT_NO_THROW(udpClient2.send(sendTest::someString, [](AddrIPv6 &s) {
 		return methods::construct(s, "::1", 16000);
 	}));
 
 	tcpServerThreadIPv6.join();
 	udpServerThreadIPv6.join();
+}
 }
