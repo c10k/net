5d7ca4ccdf27085c55528fce499b013e32f9c599
agauniyal <agauniyal@outlook.com>
2017-03-22 21:37:03 +0530

    Uncomment SO_DEBUG socket option test

--- test/socket_options_test.cpp
+++ test/socket_options_test.cpp
@@ -98,28 +98,28 @@ TEST(SocketOptions, Linger)
 }
 
 
-// TEST(SocketOptions, Debug)
-// {
-// 	Socket s(Domain::IPv4, Type::UDP);
+TEST(SocketOptions, Debug)
+{
+	Socket s(Domain::IPv4, Type::UDP);
 
-// 	int optval;
-// 	socklen_t optlen = sizeof(optval);
+	int optval;
+	socklen_t optlen = sizeof(optval);
 
-// 	SockOpt opt(1);
-// 	ASSERT_EQ(1, opt.getValue());
+	SockOpt opt(1);
+	ASSERT_EQ(1, opt.getValue());
 
-// 	s.setOpt(Opt::DEBUG, opt);
-// 	ASSERT_EQ(
-// 	  0, getsockopt(s.getSocket(), SOL_SOCKET, SO_DEBUG, &optval, &optlen));
-// 	ASSERT_EQ(1, optval);
+	s.setOpt(Opt::DEBUG, opt);
+	ASSERT_EQ(
+	  0, getsockopt(s.getSocket(), SOL_SOCKET, SO_DEBUG, &optval, &optlen));
+	ASSERT_EQ(1, optval);
 
-// 	optval = 0;
-// 	ASSERT_EQ(0,
-// 	          setsockopt(s.getSocket(), SOL_SOCKET, SO_DEBUG, &optval, optlen));
+	optval = 0;
+	ASSERT_EQ(0,
+	          setsockopt(s.getSocket(), SOL_SOCKET, SO_DEBUG, &optval, optlen));
 
-// 	auto s2 = s.getOpt(Opt::DEBUG);
-// 	ASSERT_EQ(0, s2.getValue());
-// }
+	auto s2 = s.getOpt(Opt::DEBUG);
+	ASSERT_EQ(0, s2.getValue());
+}
 
 
 TEST(SocketOptions, DontRoute)
